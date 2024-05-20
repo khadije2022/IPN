611d9084b49547c82ae9a7f02ae1d3106c0ab17a
mustapha <mustaphamed2004@gmail.com>
2024-05-20 12:07:08 +0000

    routes/web.php

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\BonSortieAchatController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\CatelogueProduitController;
+use App\Http\Controllers\DetailsMouvementController;
+use App\Http\Controllers\MagasinController;
+use App\Http\Controllers\MouvementStockController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\StockController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+use function Termwind\render;
+
+Route::redirect('/','/dashboard');
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth','verified'])->group(function() {
+    Route::get('/dashboard', fn() => Inertia::render('Dashboard'))
+    ->name('dashboard');
+
+    Route::resource('magasin',MagasinController::class);
+    Route::resource('catelogueProduit',CatelogueProduitController::class);
+    Route::resource('categorie',CategorieController::class);
+    Route::resource('mouvmentStock',MouvementStockController::class);
+    Route::resource('detailsMouvment',DetailsMouvementController::class);
+    Route::resource('bonsortieAchat',BonSortieAchatController::class);
+    Route::resource('stock',StockController::class);
+
+
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
