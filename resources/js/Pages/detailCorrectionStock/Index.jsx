b8beb9fa24ea4b98c100d0983bf435b2a251ef68
mustapha <mustaphamed2004@gmail.com>
2024-07-11 11:41:09 +0000

    the last

--- resources/js/Pages/detailCorrectionStock/Index.jsx
+++ resources/js/Pages/detailCorrectionStock/Index.jsx
@@ -17,7 +17,7 @@ function Index({ auth, detailsexpresionbesoins, success }) {
       header={
         <div className='flex justify-between items-center'>
           <h2 className='font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight'>
-          
+
           </h2>
         </div>
       }
