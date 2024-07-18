0fd6c3413702832b46d487109c78e884bc9db744
Mustapha Muhammed <129334579+mustapha2200@users.noreply.github.com>
2024-07-18 09:39:30 +0000

    Update Index.jsx

--- resources/js/Pages/CatelogueProduit/Index.jsx
+++ resources/js/Pages/CatelogueProduit/Index.jsx
@@ -7,16 +7,15 @@ import InputError from '@/Components/InputError';
 import Pagination from '@/Components/Pagination';
 import SelectInput from '@/Components/SelectInput';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrashAlt, faPlus, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrashAlt, faPlus, faFileExcel, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
-function Index({ auth, produits, categories, success, ent, queryParams = null }) {
+function Index({ auth, produits, categories, success, ent, queryParams }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalMode, setModalMode] = useState('add'); // 'add' or 'edit'
   const [currentProduit, setCurrentProduit] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState(queryParams?.product_name || '');
   const [startDate, setStartDate] = useState(queryParams?.start_date || '');
   const [endDate, setEndDate] = useState(queryParams?.end_date || '');
-  const [filteredProduits, setFilteredProduits] = useState(ent.data);
   const [sortConfig, setSortConfig] = useState({ key: 'id', direction: 'ascending' });
   const [successMessage, setSuccessMessage] = useState(success);
   const [validationErrors, setValidationErrors] = useState({});
@@ -36,20 +35,6 @@ function Index({ auth, produits, categories, success, ent, queryParams = null })
       return () => clearTimeout(timer);
     }
   }, [success]);
-
-  useEffect(() => {
-    let filtered = ent.data;
-
-    if (searchQuery) {
-      filtered = filtered.filter(produit =>
-        produit.product_name.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-    }
-
-    setFilteredProduits(filtered);
-  }, [searchQuery, ent.data]);
-
-  const handleSearchChange = (e) => setSearchQuery(e.target.value);
 
   const deleteProduit = (produit) => {
     if (!confirm('Êtes-vous sûr de vouloir supprimer cette catégorie?')) {
@@ -81,6 +66,7 @@ function Index({ auth, produits, categories, success, ent, queryParams = null })
   const closeModal = () => {
     setIsModalOpen(false);
     reset();
+    createInertiaApp.reload();
   };
 
   const validateForm = () => {
@@ -105,20 +91,25 @@ function Index({ auth, produits, categories, success, ent, queryParams = null })
     );
   };
 
-  const handleDateChange = (setter) => (e) => setter(e.target.value);
+  const handleSearchChange = (e) => setSearchQuery(e.target.value);
 
-  const handleFilterByDate = () => {
-    router.get(route('catelogueProduit.index'), {
-      start_date: startDate,
-      end_date: endDate,
-    });
-  };
+  const handleDateChange = (setter) => (e) => setter(e.target.value);
 
   const handleSort = (key) =>
     setSortConfig({
       key,
       direction: sortConfig.key === key && sortConfig.direction === 'ascending' ? 'descending' : 'ascending',
     });
+
+  const filteredProduits = ent.data;
+
+  const handleSearch = () => {
+    router.get(route('catelogueProduit.index'), {
+      product_name: searchQuery,
+      start_date: startDate,
+      end_date: endDate,
+    });
+  };
 
   return (
     <AuthenticatedLayout
@@ -171,16 +162,10 @@ function Index({ auth, produits, categories, success, ent, queryParams = null })
                 </div>
                 <div className='flex items-center space-x-2 mt-2 sm:mt-0'>
                   <button
-                    onClick={handleFilterByDate}
+                    onClick={handleSearch}
                     className='bg-emerald-500 text-white px-4 py-2 rounded shadow hover:bg-emerald-600 transition-all'
                   >
-                    Filtrer par Date
-                  </button>
-                  <button
-                    onClick={() => openModal('add')}
-                    className='bg-emerald-500 py-2 px-4 text-white rounded shadow transition-all hover:bg-emerald-600 flex items-center'
-                  >
-                    <FontAwesomeIcon icon={faPlus} className='mr-2' /> Ajouter
+                    Rechercher
                   </button>
                 </div>
               </div>
@@ -188,6 +173,21 @@ function Index({ auth, produits, categories, success, ent, queryParams = null })
                 <table className='min-w-full bg-white dark:bg-gray-800'>
                   <thead className='bg-gray-50 dark:bg-gray-700'>
                     <tr>
+                      {/* <th
+                        className='px-3 py-3 cursor-pointer'
+                        onClick={() => handleSort('product_id')}
+                      >
+                        ID
+                        {sortConfig.key === 'product_id' && (
+                          <FontAwesomeIcon
+                            icon={
+                              sortConfig.direction === 'ascending'
+                                ? faSortUp
+                                : faSortDown
+                            }
+                          />
+                        )}
+                      </th> */}
                       <th
                         className='px-3 py-3 cursor-pointer'
                         onClick={() => handleSort('product_name')}
@@ -250,10 +250,10 @@ function Index({ auth, produits, categories, success, ent, queryParams = null })
                       </th>
                       <th
                         className='px-3 py-3 cursor-pointer'
-                        onClick={() => handleSort('created_at')}
+                        onClick={() => handleSort('total_sortie')}
                       >
                         Sortie
-                        {sortConfig.key === 'created_at' && (
+                        {sortConfig.key === 'total_sortie' && (
                           <FontAwesomeIcon
                             icon={
                               sortConfig.direction === 'ascending'
@@ -263,135 +263,127 @@ function Index({ auth, produits, categories, success, ent, queryParams = null })
                           />
                         )}
                       </th>
-                      <th className='px-3 py-3'>Actions</th>
+                      <th className='px-3 py-3 text-right'>Action</th>
                     </tr>
                   </thead>
                   <tbody>
                     {filteredProduits.map((produit) => (
                       <tr
                         key={produit.product_id}
-                        className='border-b dark:border-gray-700'
+                        className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'
                       >
+                        {/* <td className='px-3 py-2'>{produit.product_id}</td> */}
                         <td className='px-3 py-2'>{produit.product_name}</td>
                         <td className='px-3 py-2'>{produit.category_name}</td>
                         <td className='px-3 py-2'>{produit.total_entree}</td>
-                        <td className='px-3 py-2'>{produit.stock <0 ? -produit.stock : produit.stock}</td>
-                        <td className='px-3 py-2'>{produit.total_sortie < 0 ? -produit.total_sortie :produit.total_sortie }</td>
-
-                        <td className='px-3 py-2 flex space-x-2'>
+                        <td className='px-3 py-2'>{produit.stock}</td>
+                        <td className='px-3 py-2'>
+                          {produit.total_sortie ? -produit.total_sortie : produit.total_sortie}
+                        </td>
+                        <td className='px-3 py-2 text-right flex justify-end'>
                           <button
                             onClick={() => openModal('edit', produit)}
-                            className='text-blue-500 hover:text-blue-700'
+                            className='text-blue-600 dark:text-blue-500 mx-1 flex items-center'
                           >
-                            <FontAwesomeIcon icon={faEdit} />
+                            <FontAwesomeIcon icon={faEdit} className='mr-1' />
                           </button>
                           <button
                             onClick={() => deleteProduit(produit)}
-                            className='text-red-500 hover:text-red-700'
+                            className='text-red-600 dark:text-red-500 mx-1 flex items-center'
                           >
-                            <FontAwesomeIcon icon={faTrashAlt} />
+                            <FontAwesomeIcon icon={faTrashAlt} className='mr-1' />
                           </button>
                         </td>
                       </tr>
                     ))}
+
                   </tbody>
                 </table>
               </div>
-              <Pagination className='mt-6' links={ent.links} />
+              <Pagination links={ent.links} />
             </div>
           </div>
         </div>
       </div>
 
       {isModalOpen && (
-        <div className='fixed z-10 inset-0 overflow-y-auto'>
-          <div className='flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
-            <div className='fixed inset-0 transition-opacity' aria-hidden='true'>
-              <div className='absolute inset-0 bg-gray-500 opacity-75'></div>
-            </div>
-            <span
-              className='hidden sm:inline-block sm:align-middle sm:h-screen'
-              aria-hidden='true'
+        <div className='fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center'>
+          <div className='relative p-5 border w-11/12 sm:w-96 shadow-lg rounded-md bg-white dark:bg-gray-800'>
+            <form
+              onSubmit={handleFormSubmit}
+              className='p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg'
             >
-              &#8203;
-            </span>
-            <div
-              className='inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6'
-              role='dialog'
-              aria-modal='true'
-              aria-labelledby='modal-headline'
-            >
-              <form onSubmit={handleFormSubmit}>
-                <div>
-                  <div className='text-lg leading-6 font-medium text-gray-900 dark:text-gray-200 mb-2'>
-                    {modalMode === 'add' ? 'Ajouter Produit' : 'Modifier Produit'}
+              <div className='mt-4'>
+                <InputLabel htmlFor='designation' value='Nom du Produit' />
+                <TextInput
+                  type='text'
+                  name='designation'
+                  id='designation'
+                  value={data.designation}
+                  className='mt-1 block w-full'
+                  onChange={(e) => setData('designation', e.target.value)}
+                />
+                {validationErrors.designation && (
+                  <div className='text-red-500 mt-2'>
+                    {validationErrors.designation}
                   </div>
-                  <div className='mb-4'>
-                    <InputLabel forInput='designation' value='Désignation' />
-                    <TextInput
-                      id='designation'
-                      name='designation'
-                      value={data.designation}
-                      className='mt-1 block w-full'
-                      autoComplete='designation'
-                      isFocused={true}
-                      handleChange={(e) => setData('designation', e.target.value)}
-                      required
-                    />
-                    <InputError message={validationErrors.designation} className='mt-2' />
+                )}
+                <InputError message={errors.designation} className='mt-2' />
+              </div>
+              <div className='mt-4'>
+                <InputLabel htmlFor='date' value='Date de Création' />
+                <TextInput
+                  type='date'
+                  name='created_at'
+                  id='date'
+                  value={data.created_at}
+                  className='mt-1 block w-full'
+                  onChange={(e) => setData('created_at', e.target.value)}
+                />
+                {validationErrors.created_at && (
+                  <div className='text-red-500 mt-2'>
+                    {validationErrors.created_at}
                   </div>
-                  <div className='mb-4'>
-                    <InputLabel forInput='type' value='Catégorie' />
-                    <SelectInput
-                      id='type'
-                      name='type'
-                      value={data.type}
-                      className='mt-1 block w-full'
-                      autoComplete='type'
-                      handleChange={(e) => setData('type', e.target.value)}
-                      required
-                    >
-                      <option value=''>Sélectionnez une catégorie</option>
-                      {categories.data.map((category) => (
-                        <option key={category.category_id} value={category.category_id}>
-                          {category.category_name}
-                        </option>
-                      ))}
-                    </SelectInput>
-                    <InputError message={validationErrors.type} className='mt-2' />
-                  </div>
-                  <div className='mb-4'>
-                    <InputLabel forInput='created_at' value='Date de Création' />
-                    <TextInput
-                      id='created_at'
-                      type='date'
-                      name='created_at'
-                      value={data.created_at}
-                      className='mt-1 block w-full'
-                      autoComplete='created_at'
-                      handleChange={(e) => setData('created_at', e.target.value)}
-                      required
-                    />
-                    <InputError message={validationErrors.created_at} className='mt-2' />
-                  </div>
-                </div>
-                <div className='mt-5 sm:mt-6'>
-                  <button
-                    type='submit'
-                    className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-emerald-500 text-base font-medium text-white hover:bg-emerald-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 sm:text-sm'
-                  >
-                    {modalMode === 'add' ? 'Ajouter' : 'Modifier'}
-                  </button>
-                  <button
-                    type='button'
-                    className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 sm:mt-0 sm:text-sm'
-                    onClick={closeModal}
-                  >
-                    Annuler
-                  </button>
-                </div>
-              </form>
-            </div>
+                )}
+                <InputError message={errors.created_at} className='mt-2' />
+              </div>
+              <div className='mt-4'>
+                <InputLabel htmlFor='type' value='Catégorie' />
+                <SelectInput
+                  name='type'
+                  id='type'
+                  className='mt-1 block w-full'
+                  value={data.type}
+                  onChange={(e) => setData('type', e.target.value)}
+                >
+                  <option value=''>Sélectionnez une categories </option>
+                  {categories.data.map((category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.type}
+                    </option>
+                  ))}
+                </SelectInput>
+                {validationErrors.type && (
+                  <div className='text-red-500 mt-2'>{validationErrors.type}</div>
+                )}
+                <InputError message={errors.type} className='mt-2' />
+              </div>
+              <div className='mt-4 text-right'>
+                <button
+                  type='button'
+                  onClick={closeModal}
+                  className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2'
+                >
+                  Annuler
+                </button>
+                <button
+                  type='submit'
+                  className='bg-emerald-500 py-2 px-4 text-white rounded shadow transition-all hover:bg-emerald-600'
+                >
+                  {modalMode === 'add' ? 'Ajouter' : 'Sauvegarder'}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       )}
